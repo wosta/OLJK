c29e4008c1371a100abca6dd17fb081e3be36898
weiwang <weiwang1202@126.com>
2016-04-14 22:53:29 +0800

    1.修改

---
+++ 里程碑.m
@@ -0,0 +1,12 @@
+作业
+1.注册自己的github账号，并把上期的作业上传到git
+https://github.com/wosta/OLJK.git
+
+2、注册自己的cocoaChina账号，并在里面发表一些评论甚至自己开启一个话题
+http://www.cocoachina.com/ios/20160408/15868.html
+
+
+RootViewController 里的[self kvo]; 运行这个方法，点击next按钮之后进入下一页然后再返回就crash了！求老师帮我看下什么原因，谢谢。
+
+https://github.com/wosta/OLKVO-KVC.git
+
